417311e03161960e68ed898f4af70c4cd3581055
tandejun <dejuntan1@gmail.com>
2025-07-20 17:12:28 +0800

    D3: Added plugin security

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,21 @@
-import js from "@eslint/js";
 import globals from "globals";
+import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
+import pluginSecurity from "eslint-plugin-security";
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+{files: ["**/*.{js,mjs,cjs,jsx}"]},
+{languageOptions: { globals: globals.browser }},
+pluginJs.configs.recommended,
+pluginReact.configs.flat.recommended,
+{
+plugins: {
+security: pluginSecurity
+},
+rules: {
+...pluginJs.configs.recommended.rules,
+...pluginReact.configs.flat.recommended.rules,
+"security/detect-eval-with-expression": "error",
+}
+}
+];
