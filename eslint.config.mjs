a92df67503c810da75a4e9e3f5de04387849732e
tandejun <dejuntan1@gmail.com>
2025-07-20 17:28:37 +0800

    D3: Test with original config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,11 @@
+import js from "@eslint/js";
 import globals from "globals";
-import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
-import pluginSecurity from "eslint-plugin-security";
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-{files: ["**/*.{js,mjs,cjs,jsx}"]},
-{languageOptions: { globals: globals.browser }},
-pluginJs.configs.recommended,
-pluginReact.configs.flat.recommended,
-{
-plugins: {
-security: pluginSecurity
-'no-unsanitized': noUnsanitized
-},
-rules: {
-...pluginJs.configs.recommended.rules,
-...pluginReact.configs.flat.recommended.rules,
-"security/detect-eval-with-expression": "error",
-'no-unsanitized/method': 'error',
-      'no-unsanitized/property': 'error'
-}
-}
-];
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
+]);
